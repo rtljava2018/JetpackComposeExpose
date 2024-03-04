ddec4edfdd12763332cb75598e71fc9d30448d9d
RAMKRISHNA <ramkrishna.uds@gmail.com>
2024-03-04 20:31:28 +0530

    remove import

--- app/src/main/java/com/demo/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/demo/jetpackcomposedemo/MainActivity.kt
@@ -8,15 +8,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import com.demo.jetpackcomposedemo.coreui.AppNavigationGraph
-import com.demo.jetpackcomposedemo.presenter.movieslist.MainMoviesListScreen
-import com.demo.jetpackcomposedemo.presenter.movieslist.MainMoviesViewModel
-import com.demo.jetpackcomposedemo.presenter.movieslist.MoviesScreen
 import com.demo.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
